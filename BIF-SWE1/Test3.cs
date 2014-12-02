a1bc3537b7393f65af0d2679c9f7149624431581
Arthur Zaczek <arthur@dasz.at>
2014-12-02 15:42:04 +0100

    Initial commit

---
+++ BIF-SWE1/Test3.cs
@@ -0,0 +1,37 @@
+﻿using BIF.SWE1.Exam.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIF.SWE1.Exam
+{
+    public class Test3 : ITest3
+    {
+        public object Method1(int i, string str, object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Method2(int i, string str, object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Method3(int i, string str, object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Method4(int i, string str, object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Method5(int i, string str, object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
